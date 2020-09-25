e6308a3b81251f1acf2712514826bb578d80c124
Edycar Reyes <edycarreyes@gmail.com>
2020-09-25 17:37:48 -0600

    Se crean los temas a utilizarse en la aplicacion

---
+++ lib/themes/dark-theme.dart
@@ -0,0 +1,8 @@
+import 'package:flutter/material.dart';
+
+ThemeData darkTheme = ThemeData(
+  primaryColor: Colors.grey[900],
+  primarySwatch: Colors.blue,
+  accentColor: Colors.blue,
+  visualDensity: VisualDensity.adaptivePlatformDensity,
+);
