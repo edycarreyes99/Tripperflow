539a4b4e4ed6599a367ffbe6007ec0a7ac5dd811
Edycar Reyes <edycarreyes@gmail.com>
2020-09-27 18:17:10 -0600

    Se crea el evento principal para hacer un fetch de los datos desde firestore

--- lib/blocs/capitales/capitales_event.dart
+++ lib/blocs/capitales/capitales_event.dart
@@ -3,3 +3,9 @@ part of 'capitales_bloc.dart';
 abstract class CapitalesEvent extends Equatable {
   const CapitalesEvent();
 }
+
+
+class OnFetchCapitales extends CapitalesEvent {
+  @override
+  List<Object> get props => [];
+}
