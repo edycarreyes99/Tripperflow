3dd596eaa5a164e47b347e6e37f0a386eec4203e
Edycar Reyes <edycarreyes@gmail.com>
2020-09-27 18:16:37 -0600

    Se crean eventos de prueba para simular un fetch de los datos desde firestore

--- lib/blocs/capitales/capitales_bloc.dart
+++ lib/blocs/capitales/capitales_bloc.dart
@@ -2,17 +2,31 @@ import 'dart:async';
 
 import 'package:bloc/bloc.dart';
 import 'package:equatable/equatable.dart';
+import 'package:flutter/material.dart';
 
 part 'capitales_event.dart';
+
 part 'capitales_state.dart';
 
 class CapitalesBloc extends Bloc<CapitalesEvent, CapitalesState> {
-  CapitalesBloc() : super(CapitalesInitial());
+  final BuildContext context;
+
+  CapitalesBloc({this.context}) : super(CapitalesInitialState());
 
   @override
   Stream<CapitalesState> mapEventToState(
     CapitalesEvent event,
   ) async* {
-    // TODO: implement mapEventToState
+    switch (event.runtimeType) {
+      case OnFetchCapitales:
+        yield FetchingCapitalesState();
+
+        await Future.delayed(Duration(seconds: 2));
+
+        yield EmptyCapitalesState();
+        break;
+    }
   }
+
+  String getNombre() => "HOla mundo";
 }
